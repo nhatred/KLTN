e2d7baf8cf695ec9abfde45d8abae959dd2d621c
ToanJt <lexuantoan5703@gmail.com>
2025-04-17 03:48:31 +0700

    update

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Database connected");
   });
-  await mongoose.connect(`${process.env.MONGODB_URI}/Squizz`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/Cluster0`);
 };
 
 export default connectDB;
---
+++ server/testDb.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mongodb from "./configs/mongodb.js";
+
+mongodb();
