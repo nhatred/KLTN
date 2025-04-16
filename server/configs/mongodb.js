3f871b14382e67961ba12c9b8c0a6098596aa42b
ToanJt <lexuantoan5703@gmail.com>
2025-04-17 03:59:11 +0700

    aaaa

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("Database connected");
-  });
-  console.log("🚀 Bắt đầu kết nối MongoDB...");
-  await mongoose.connect(`${process.env.MONGODB_URI}/squizz`);
-  console.log("✅ Kết nối MongoDB thành công!");
+  try {
+    console.log("📡 Đang kết nối đến MongoDB...");
+    await mongoose.connect(`${process.env.MONGODB_URI}/squizz`);
+    console.log("✅ Kết nối MongoDB thành công!");
+
+    mongoose.connection.on("connected", () => {
+      console.log("🔌 Mongoose đã kết nối");
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.error("❌ Mongoose báo lỗi:", err);
+    });
+  } catch (error) {
+    console.error("❌ Lỗi khi kết nối MongoDB:", error);
+    throw error; // ném lỗi để hàm gọi nó biết
+  }
 };
 
 export default connectDB;
