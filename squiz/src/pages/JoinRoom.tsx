3e890aff2c873394cecfbe411b5a5bb220d69838
ToanJt <lexuantoan5703@gmail.com>
2025-05-18 02:16:58 +0700

    Trước khi code xử lý quizRoom

--- squiz/src/pages/JoinRoom.tsx
+++ squiz/src/pages/JoinRoom.tsx
@@ -21,8 +21,8 @@ export default function JoinRoom() {
     };
 
     return (
-        <div className="bg-gray-900 h-screen text-background">
-            <nav className="flex bg-black justify-between items-center px-8 py-2">
+        <div className="min-h-screen bg-gradient-to-b from-black via-gray-900 to-littleblue text-background">
+            <nav className="flex justify-between items-center px-8 py-2">
                 <h1 className="text-3xl font-black">Squizz</h1>
                 <div 
                     className="flex bg-orange text-darkblue btn-hover items-center gap-2 py-2 px-3 rounded font-semibold text-lg cursor-pointer"
@@ -57,7 +57,7 @@ export default function JoinRoom() {
             )}
 
             <main className="flex flex-col justify-center items-center mt-10">
-                <div className="bg-black p-6 rounded-lg w-2/5 ">
+                <div className="bg-black/50 backdrop-blur-sm p-6 rounded-lg w-2/5">
                     <div className="flex items-center justify-between gap-2">
                         <p className="text-xl font-semibold">Hướng dẫn tham gia phòng</p>
                         <p className="text-darkblue font-bold btn-hover cursor-pointer flex items-center gap-2 text-sm bg-orange  p-2 rounded-lg">
@@ -86,10 +86,10 @@ export default function JoinRoom() {
                         </div>
                     </div>
                 </div>
-                <div className="w-full bg-littleblue h-screen my-16">
-                    <div className="flex relative items-center justify-center">
+                <div className="w-full my-16">
+                    <div className="flex relative items-center justify-center border-b-1 border-gray-800">
                         <div className="flex absolute left-5 items-center justify-center rounded-lg gap-2">
-                            <div className="flex text-background border-2 border-gray-600 items-center gap-2 bg-black cursor-pointer btn-hover p-2 px-5 rounded-lg">
+                            <div className="flex text-background border-2 border-gray-600 items-center gap-2 bg-black/50 backdrop-blur-sm cursor-pointer btn-hover p-2 px-5 rounded-lg">
                                  <HugeiconsIcon icon={UserGroup03Icon} />
                             <p className="text-lg font-semibold">10</p>
                            </div>
@@ -101,7 +101,7 @@ export default function JoinRoom() {
                         </div>
                     </div>
                     <div className="flex items-center justify-center mt-20 grid grid-cols-5 gap-5 mx-8">
-                        <div className="flex relative h-full items-center justify-start gap-3 bg-[#384052] px-3 py-5 rounded-lg group">
+                        <div className="flex relative h-full items-center justify-start gap-3 bg-[#384052]/50 backdrop-blur-sm px-3 py-5 rounded-lg group">
                             <div className="w-14 h-14 rounded-full overflow-hidden flex items-center justify-center">
                                 <img className=" object-cover" src="https://images.unsplash.com/photo-1574232877776-2024ccf7c09e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHVzZXJ8ZW58MHx8MHx8fDA%3D" alt="" />
                             </div>
@@ -110,7 +110,7 @@ export default function JoinRoom() {
                                 <p className="text-sm font-semibold text-darkblue">Nhấp để xóa thí sinh</p>
                             </div>
                         </div>
-                        <div className="flex relative h-full items-center justify-start gap-3 bg-[#384052] px-3 py-5 rounded-lg group">
+                        <div className="flex relative h-full items-center justify-start gap-3 bg-[#384052]/50 backdrop-blur-sm px-3 py-5 rounded-lg group">
                            <div className="w-14 h-14 rounded-full overflow-hidden flex items-center justify-center">
                                 <img className=" object-cover" src="https://images.unsplash.com/photo-1574232877776-2024ccf7c09e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHVzZXJ8ZW58MHx8MHx8fDA%3D" alt="" />
                             </div>
@@ -119,10 +119,9 @@ export default function JoinRoom() {
                                 <p className="text-sm font-semibold text-darkblue">Nhấp để xóa thí sinh</p>
                             </div>
                         </div>
-                        
                     </div>
                 </div>
             </main>
         </div>
-    )
+    );
 }
